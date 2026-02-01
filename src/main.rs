8635f537c20723f89b6983a960f03a66d82d2779
u_u <nowhere.rev2@gmail.com>
2026-02-01 16:37:23 +0900

    parse --by arg

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,25 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
+    if args.len() != 4 {
         eprintln!("Usage: logsum <path> --by status|path");
         return;
     }
 
     let path = &args[1];
+    if args[2] != "--by" || (args[3] != "status" && args[3] != "path") {
+        eprintln!("Usage: logsum <path> --by status|path");
+        return;
+    }
     match fs::read_to_string(path) {
-        Ok(_) => println!("ok: loaded file {}", path),
+        Ok(content) => {
+            for line in content.lines() {
+                let parts: Vec<&str> = line.split_whitespace().collect();
+                if parts.len() != 4 {
+                    continue;
+                }
+            }
+        }
         Err(e) => eprintln!("error: could not read file {}: {}", path, e),
     }
 }
