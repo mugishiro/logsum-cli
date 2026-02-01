dea8c9e83800fca5cea9af94034b6ad424eb7ed1
u_u <nowhere.rev2@gmail.com>
2026-02-01 16:29:19 +0900

    check file can be loaded

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        eprintln!("Usage: logsum <path> by status|path");
+        eprintln!("Usage: logsum <path> --by status|path");
         return;
+    }
+
+    let path = &args[1];
+    match fs::read_to_string(path) {
+        Ok(_) => println!("ok: loaded file {}", path),
+        Err(e) => eprintln!("error: could not read file {}: {}", path, e),
     }
 }
